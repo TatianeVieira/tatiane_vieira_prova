336fa1f10d0e4f0fe85840c6d57ae2979312c90d
TatianeVieira <tatiane.vieira.bruder@gmail.com>
2025-08-11 17:03:47 -0300

    material para prova
    recuperar senha

--- MaterialApoio/recuperar_senha.php
+++ MaterialApoio/recuperar_senha.php
@@ -27,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
         //simula o envio do email (grava em txt)
         simularEnvioEmail($email, $senha_temporaria);
-        echo "<script>alert('Uma senha temporaria foi gerada e enviada (simulacao).Verifique o arquivo emails_simulados.txt'); window.location.href='login.php';</script>";
+        echo "<script>alert('Uma senha temporaria foi gerada e enviada (simulacao).Verifique o arquivo emails_simulados.txt'); window.location.href='index.php';</script>";
     }else{
         echo "<script>alert('Email nao encontrado');</script>";
     }
